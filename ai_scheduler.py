3b85534596f930b09c5796152b36891e578df1a4
AKSHAT PRATAP SINGH <ersinghakshat01@gmail.com>
2025-04-17 14:14:14 +0530

    Remove

--- ai_scheduler.py
+++
@@ -1,7 +0,0 @@
-# ai_scheduler.py
-class AIScheduler:
-    def predict_best_algorithm(self, processes):
-        if not processes:
-            return "FCFS"
-        avg_burst = sum(p.burst_time for p in processes) / len(processes)
-        return "SJF-P" if avg_burst <= 5 else "RR"
